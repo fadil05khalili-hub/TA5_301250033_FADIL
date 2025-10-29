0c2b0beddc1b953ede822dcb028ca1243a0a38e9
fadil05khalili-hub <fadil05khalili@gmail.com>
2025-10-29 12:27:05 +0700

    TUGAS_MANDIRI_5
    Di bawah langit senja yang perlahan memudar,
    Aku menulis namamu di atas angin,
    Agar semesta tahu, ada rindu yang tak bersuara,
    Namun tumbuh di dada, seperti bunga di musim tenang.

---
+++ pola_angka_menurun.py
@@ -0,0 +1,4 @@
+for i in range(5, 0, -1):      # dari 5 turun ke 1
+    for j in range(1, i + 1):  # cetak angka dari 1 sampai i
+        print(j, end="")
+    print()                    # pindah ke baris baru
---
+++ pola_segitigakiri.py
@@ -0,0 +1,4 @@
+for i in range(1, 6):          # dari 1 sampai 5
+    for j in range(1, i + 1):  # cetak bintang sebanyak i
+        print("*", end="")
+    print()                    # ganti baris
